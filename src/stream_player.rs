eb38bd6fba4d021a2c16f6fe25b5500e2e5a406c
m-ceta <moto@PCISDM0193.IMAZO.local>
2021-05-28 18:14:32 +0900

    Adjusted error message.

--- src/stream_player.rs
+++ src/stream_player.rs
@@ -12,14 +12,15 @@ pub fn play(url: &String, auth_token: &String) -> Result<(Child, Child), String>
     let mut command2 = Command::new("ffplay");
     let args2 = ["-i", "-"];
     if let Ok(mut proc1) = command1.args(&args1).stdout(Stdio::piped()).spawn() {
-        let pipe = stdout_to_stdin(&proc1).unwrap();
-        if let Ok(proc2) = command2.args(&args2).stdout(pipe).spawn() {
-            return Ok((proc1, proc2));
-        } 
-        proc1.kill().unwrap();
+        if let Ok(pipe) = stdout_to_stdin(&proc1) {
+            if let Ok(proc2) = command2.args(&args2).stdout(pipe).spawn() {
+                return Ok((proc1, proc2));
+            } 
+        }
+        proc1.kill().unwrap_or("Failed to kill ffmpeg.");
         return Err(String::from("Failed to execute ffplay."));
     } 
-    Err(String::from("Failed to execute ffmpg."))
+    Err(String::from("Failed to execute ffmpeg."))
 }
 
 fn stdout_to_stdin(process: &Child) -> Option<Stdio> {
