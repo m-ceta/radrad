5397bc522df6b213dd89258187bd5aeffcd084a1
Motohiro Seta <motohiro.seta@gmail.com>
2021-05-30 10:20:18 +0900

    bug fix.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,20 @@ extern crate reqwest;
 extern crate quick_xml;
 extern crate regex;
 extern crate base64;
-extern crate signal_hook;
 
-use signal_hook::iterator::Signals;
-use signal_hook::consts::signal::*;
-use std::thread;
-use std::process::Command;
 use std::env;
 
 mod rd_client;
 mod stream_player;
+
+#[cfg(target_os = "linux")]
+extern crate signal_hook;
+#[cfg(target_os = "linux")]
+use signal_hook::consts::signal::*;
+#[cfg(target_os = "linux")]
+use std::thread;
+#[cfg(target_os = "linux")]
+use std::process::Command;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -33,30 +37,8 @@ fn main() {
         }
     }
     if let Some((auth, url)) = rd_client::get_live_stream_info(&program, tf, af) {
-        if let Ok((mut proc1, mut proc2)) = stream_player::play(&url, &auth) {
-            let mut signals = Signals::new(&[SIGINT, SIGHUP, SIGQUIT, SIGKILL, SIGTERM]).unwrap();
-            let id1 = proc1.id();
-            let id2 = proc2.id();
-            thread::spawn(move || {
-                let mut flag = true;
-                for _sig in signals.forever() {
-                    if flag {
-                        Command::new("kill")
-                            .arg("-9")
-                            .arg(id1.to_string())
-                            .spawn()
-                            .ok();
-                        Command::new("kill")
-                            .arg("-9")
-                            .arg(id2.to_string())
-                            .spawn()
-                            .ok();
-                        flag = false;
-                    }
-                }
-            });
-            proc1.wait().ok();
-            proc2.wait().ok();
+        if let Ok(proc1) = stream_player::play(&url, &auth) {
+            process_wait(proc1);
         } else {
             println!("Cannnot play the program '{}'.", program);
         }
@@ -64,4 +46,29 @@ fn main() {
         println!("Not found the program '{}'.", program);
     }
 }
+
+#[cfg(target_os = "linux")]
+fn process_wait(mut proc: std::process::Child) {
+    let id = proc.id();
+    let mut signals = signal_hook::iterator::Signals::new(&[SIGINT, SIGQUIT, SIGHUP, SIGTERM, SIGTSTP]).unwrap();
+    thread::spawn(move || {
+        let mut flag = true;
+        for _sig in signals.forever() {
+            if flag {
+                Command::new("kill")
+                    .arg(id.to_string())
+                    .spawn()
+                    .ok();
+                flag = false;
+            }
+        }
+    });
+    proc.wait().ok();
+}
+
+#[cfg(target_os = "windows")]
+fn process_wait(mut proc: std::process::Child) {
+    proc.wait().ok();
+}
+
 
--- src/stream_player.rs
+++ src/stream_player.rs
@@ -1,32 +1,15 @@
 use std::process::{Child, Command, Stdio};
-use std::os::unix::io::{AsRawFd, FromRawFd};
 
-pub fn play(url: &String, auth_token: &String) -> Result<(Child, Child), String> {
-    let mut command1 = Command::new("ffmpeg");
-    let args1 = ["-y", 
-        "-headers", 
-        &format!("\"X-Radiko-AuthToken: {}\"", auth_token),
-        "-i",
-        &format!("\"{}\"", url),
-        "-"];
-    let mut command2 = Command::new("ffplay");
-    let args2 = ["-i", "-"];
-    if let Ok(mut proc1) = command1.args(&args1).stdout(Stdio::piped()).spawn() {
-        if let Some(pipe) = stdout_to_stdin(&proc1) {
-            if let Ok(proc2) = command2.args(&args2).stdout(pipe).spawn() {
-                return Ok((proc1, proc2));
-            } 
-        }
-        proc1.kill().ok();
-        return Err(String::from("Failed to execute ffplay."));
+pub fn play(url: &String, auth_token: &String) -> Result<Child, String> {
+    let mut command1 = Command::new("ffplay");
+    let args1 = ["-headers"
+        , &format!("X-Radiko-AuthToken:{}", auth_token)
+        , "-i"
+        , &format!("{}", url)
+        , "-nodisp"];
+    if let Ok(proc1) = command1.args(&args1).stdout(Stdio::piped()).spawn() {
+        return Ok(proc1);
     } 
     Err(String::from("Failed to execute ffmpeg."))
-}
-
-fn stdout_to_stdin(process: &Child) -> Option<Stdio> {
-  if let Some(ref stdout) = process.stdout {
-    return Some(unsafe { Stdio::from_raw_fd(stdout.as_raw_fd()) });
-  }
-  None
 }
 
